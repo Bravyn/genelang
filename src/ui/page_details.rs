b5cb1f1b1565020583ff8abce0ed9dd2a99a4cb4
Ian Bravyn <ianbravynsa@gmail.com>
2022-07-18 18:09:23 +0300

    added json data file

--- src/ui/page_details.rs
+++ src/ui/page_details.rs
@@ -1,10 +1,26 @@
-pub struct PageDetails{
-    pub title: String
+use std::fs;
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Page{
+    pages: Vec<PageDetails>
 }
-impl PageDetails{
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct PageDetails{
+    pub title: String,
+    pub link: bool,
+    pub path: String,
+    pub content: String,
+    pub template: bool,
+    pub index: i32,
+}
+
+
+/*impl PageDetails{
     fn new(title: String) -> PageDetails {
         PageDetails{
             title
         }
     }
-}
+}*/
