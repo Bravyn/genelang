05af15a89104b79b3dcf23f1dc8d0d6a61969f97
Ian Bravyn <ianbravynsa@gmail.com>
2022-07-20 09:00:45 +0300

    Update page_details.rs

--- src/ui/page_details.rs
+++ src/ui/page_details.rs
@@ -20,15 +20,33 @@ pub fn read_json() -> String {
     json_data
 }
 
-
 impl Page{
+
     pub fn new<'a>(json_str:&'a str) -> Page {
+
         let page = serde_json::from_str::<Page>(json_str)
         .expect("Error reading json to struct");
+        
         page   
     }
+
     pub fn read_json() -> String {
         let json_data = fs::read_to_string("./data/data.json").expect("Error reading Json file :(");
         json_data
-    }    
+    }  
+
+    pub fn html_template(page: Page) {
+        let mut template_str = "".to_string();
+
+        for i in 0..pages.len(){
+            let page_details = page.pages[i];
+            let template = page_details.template;
+
+            if template == true{
+                
+            }
+        }
+
+    }
+
 }
